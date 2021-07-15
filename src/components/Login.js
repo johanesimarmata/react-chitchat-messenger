0edf54a5f25b43082249aab4b8fe0d0eca060080
Johanes Simarmata <jmpsimarmata@gmail.com>
2021-07-15 22:48:57 +0700

    Delete Login With Facebook

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {GoogleOutlined, FacebookOutlined} from '@ant-design/icons'
+import {GoogleOutlined} from '@ant-design/icons'
 import "firebase/app"
 import {auth} from '../firebase'
 import firebase from 'firebase/app'
@@ -16,14 +16,6 @@ const Login = () => {
                     onClick={() => auth.signInWithRedirect(new firebase.auth.GoogleAuthProvider())}
                 >
                     <GoogleOutlined/> Sign In with Google
-                </div>
-                <br/><br/>
-
-                <div
-                    className='login-button facebook'
-                    onClick={() => auth.signInWithRedirect(new firebase.auth.FacebookAuthProvider())}
-                    >
-                    <FacebookOutlined/> Sign In with Facebook
                 </div>
             </div>
         </div>
